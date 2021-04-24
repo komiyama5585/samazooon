c3af5b52294dddb6f7012fc3f3b31ea174516180
komiyama5585 <7Gfra4c2@gmail.com>
2021-04-24 13:50:19 +0900

    21.4.24

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
 
 
 Route::resource('products', 'ProductController');
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');

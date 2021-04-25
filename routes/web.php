b0f77b5ff69aefd2b61d6240b4502e683895ed41
komiyama5585 <7Gfra4c2@gmail.com>
2021-04-25 21:50:18 +0900

    21.4.25

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WebController@index');
+
+Route::get('users/carts', 'CartController@index')->name('carts.index');
+
+Route::post('users/carts', 'CartController@store')->name('carts.store');
+
+Route::delete('users/carts', 'CartController@destroy')->name('carts.destroy');
 
 Route::get('users/mypage', 'UserController@mypage')->name('mypage');
 Route::get('users/mypage/edit', 'UserController@edit')->name('mypage.edit');
